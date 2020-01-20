e447a7bdb36fbdcebeadb0927cff0cf3b36e8685
Andrew Cobb <andrew@cobbs-web.com>
2020-01-20 14:20:14 -0500

    exercise 4 wk2

--- lab-wk-02/exercises02.js
+++ lab-wk-02/exercises02.js
@@ -10,6 +10,8 @@ function compareNumToTen(number){
 
 compareNumToTen(15).then(result => console.log(result))
     .catch(error => console.log(error))
+compareNumToTen(8).then(result => console.log(result))
+    .catch(error => console.log(error))
 
 //Exercise 2:
 function makeUpperCase(words){
@@ -42,6 +44,9 @@ const mixxedArray = ['anarchy', 99, true]
 makeUpperCase(arrayOfNames).then(sortWords)
     .then((result) => console.log(result))
     .catch(error => console.log(error))
+makeUpperCase(mixxedArray).then(sortWords)
+    .then((result) => console.log(result))
+    .catch(error => console.log(error))
 
 //Exercise 3
 var promise1 = 101;
@@ -53,3 +58,21 @@ var promise3 = new Promise(function(resolve, reject){
 Promise.all([promise1, promise2, promise3]).then(function(vals){
     console.log(vals);
 })
+
+//Exercise 4
+function delayedPromise(num){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(num*num);
+        })
+    })
+}
+
+delayedPromise(5).then(relult => console.log(result))
+
+async function asyncPromise(num){
+    setTimeout(() => {
+        return num*num;
+    })
+}
+asyncPromise(6).then(result => console.log(result))
