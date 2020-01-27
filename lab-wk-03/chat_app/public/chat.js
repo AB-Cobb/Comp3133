4bc1f6afb78448c48de39d8074806ebae7739eee
Andrew Cobb <andrew@cobbs-web.com>
2020-01-27 14:19:46 -0500

    chat app styling

--- lab-wk-03/chat_app/public/chat.js
+++ lab-wk-03/chat_app/public/chat.js
@@ -1,4 +1,5 @@
-$(function(){ 
+$(function(){
+    var name = "Anonymouse"
     //make connection
     var socket = io.connect('http://localhost:3000')
 
@@ -6,18 +7,22 @@ $(function(){
     var message = $("#message")
     var username = $("#username")
     var send_message = $("#send_message")
-    var send_username = $("#send_username")
+    //var send_username = $("#send_username")
     var chatroom = $("#chatroom")
-
+    /*
     //send username
     send_username.click(function(){
         console.log(username.val())
         socket.emit("change_username", {username : username.val()})
 
-    })
+    })// */
 
     //send message
     send_message.click(function (){
+        if (username.val() != name){
+            socket.emit("change_username", {username : username.val()})
+            name = username.val();
+        }
         console.log(message.val())
         socket.emit('new_message', {message : message.val(), username : username.val()})
     })
@@ -25,8 +30,8 @@ $(function(){
     //receive message
     socket.on("new_message", (data) => {
         console.log(data)
-        chatroom.append("<p class='message'>"+ data.username
-            +" : "+ data.message +"</p>")
+        chatroom.append("<div class='message'><span>"+ data.username
+            +":</span><p class='message_content'>"+ data.message +"</p></div>")
     })
 });
 
